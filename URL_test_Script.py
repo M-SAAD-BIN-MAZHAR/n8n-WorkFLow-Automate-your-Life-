71e425d74d8d7f6b85ce09bab559a0460820701a
M.Saad Bin Mazhar <msaadbinmazhar@gmail.com>
2026-02-18 00:10:52 +0500

    Change URL variable to an empty string
    Updated the URL variable to an empty string.

--- URL_test_Script.py
+++ URL_test_Script.py
@@ -1,7 +1,7 @@
 import requests
 user_message="Get the data from the google sheet and add all the expenses" 
 request_message={"message":user_message}
-url="http://localhost:5678/webhook/557d3f68-3720-499e-8419-1a45c142dbef"
+url=""
 response=requests.post(url,json=request_message)
 print(response.status_code)
 print(response.json()[0]['output'])
